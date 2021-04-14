340e6709fbea94d9c0c656393e80a493e66b509a
dmitriy <admin@admin.com>
2021-04-14 09:38:33 +0300

    some changes in admin.py

--- library/admin.py
+++ library/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from .models import Book, Author
 # Register your models here.
 
-admin.site.register(Book)
-admin.site.register(Author)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+	list_display = ('title', 'author', 'pub_date')
+	list_filter = ('author', 'pub_date',)
+	search_fields = ('title', 'text')
+	date_hierarchy = 'pub_date'
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+	search_fields = ('name', 'about')
+	
